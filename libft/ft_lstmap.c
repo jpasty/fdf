4039cd56f810677487453d21e254e295d3f77158
jpasty <49813695+jpasty@users.noreply.github.com>
2020-03-10 20:47:55 +0300

    Add files via upload
    .

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpasty <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/24 19:26:54 by jpasty            #+#    #+#             */
+/*   Updated: 2019/04/24 20:02:21 by jpasty           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list *new;
+
+	if (lst != NULL)
+	{
+		if (!(new = (t_list *)malloc(sizeof(f(lst)))))
+			return (NULL);
+		new = f(lst);
+		new->next = ft_lstmap(lst->next, f);
+		return (new);
+	}
+	return (NULL);
+}
